1da483b2c4d6db4ad4413216bbd58099b503f431
StereoFlo <stereoflo@stereoflo.ru>
2022-01-25 10:30:04 +0300

    naming fix

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,24 +3,24 @@ import { HttpService } from './http.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: HttpService) {}
+  constructor(private readonly httpService: HttpService) {}
 
   @Get()
   @Render('index')
   async root() {
-    return { message: await this.appService.getAll() };
+    return { message: await this.httpService.getAll() };
   }
 
   @Get(':id')
   @Render('thread-list')
   async getThreadList(@Param('id') id: string) {
-    return { list: await this.appService.getThreadList(id) };
+    return { list: await this.httpService.getThreadList(id) };
   }
 
   @Get(':id/:num')
   @Render('thread-view')
   async getThread(@Param('id') id: string, @Param('num') num: number) {
-    const posts = await this.appService.getThread(id, num);
+    const posts = await this.httpService.getThread(id, num);
     const first = posts.posts[0];
     delete posts.posts[0];
     return {
