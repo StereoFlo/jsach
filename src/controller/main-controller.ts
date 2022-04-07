f9528b682d9733fe999bf25327633ae22bae53a9
StereoFlo <stereoflo@users.github.com>
2022-04-07 13:12:32 +0300

    fixes

--- src/controller/main-controller.ts
+++ src/controller/main-controller.ts
@@ -1,4 +1,4 @@
-import { Get, Controller, Render, Param, Query } from '@nestjs/common';
+import { Controller, Get, Param, Query, Render } from '@nestjs/common';
 import { HttpService } from '../service/http.service';
 
 @Controller()
@@ -12,13 +12,16 @@ export class MainController {
 
   @Get()
   @Render('index')
-  async root() {
+  async index(): Promise<any> {
     return { message: await this.httpService.getAll() };
   }
 
   @Get(':id')
   @Render('thread-list')
-  async getThreadList(@Param('id') id: string, @Query('page') page = 1) {
+  async getThreadList(
+    @Param('id') id: string,
+    @Query('page') page = 1,
+  ): Promise<any> {
     const list = await this.httpService.getThreadList(id, page);
     const paginationStart = list.pages.slice(0, 3);
     const paginationEnd = list.pages.slice(
@@ -26,7 +29,7 @@ export class MainController {
       list.pages.length,
     );
 
-    return { list, paginationStart, paginationEnd };
+    return { list, paginationStart, paginationEnd, title: list.BoardInfo };
   }
 
   @Get(':id/:num')
@@ -36,7 +39,7 @@ export class MainController {
     @Param('num') num: number,
     @Query('page') page = 1,
     @Query('limit') limit = 10,
-  ) {
+  ): Promise<any> {
     const thread = await this.httpService.getThread(id, num);
     const threads = thread.threads[0];
     const first = threads.posts[0];
@@ -49,6 +52,8 @@ export class MainController {
       pages.push(i);
     }
     return {
+      title: first.subject,
+      thread,
       first,
       posts,
       pagination: {
